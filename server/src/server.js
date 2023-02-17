96dbdbca220e6ff9bbc1810d11365dc5b920f643
Ricardo <ricardosala@grupotesys.es>
2023-02-17 17:26:48 +0100

    planets controller and router wip

---
+++ server/src/server.js
@@ -0,0 +1,21 @@
+// import express from 'express' --> we are gonna try alternative way with the built-in node module for servers
+// const app = express()
+// app.listen()
+
+import http from 'http'
+
+import app from './app.js'
+
+
+const PORT = process.env.PORT || 8000
+
+const server = http.createServer( app ) //It's an alternative way to do it, but the result is the same. Express is just a fancy listener function for the builtin http server
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})
+
+
+
+
+console.log(PORT);
